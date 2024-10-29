280082c2f2f221282979b3f55e7c167bd0d86c1b
Akram Arilaz Maouche <akram.maouche07@gmail.com>
2024-10-29 15:29:29 +0100

    pdf file

---
+++ app.py
@@ -0,0 +1,18 @@
+# Steps
+# Filed to put my my JD (Job Description)
+# Upload Pdf (Resume)
+# Pdf to Image ----> Processing ----> google Gemini Pro 
+# Prompt template[multiple prompt]
+
+from dotenv import load_dotenv 
+import streamlit as st 
+import os 
+from PL import Image
+import pfd2image 
+import google.generativeai as genai 
+
+load_dotenv() 
+
+
+
+
